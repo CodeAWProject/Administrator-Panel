c5ba56ef71bca7b77b20f6e88ccde3fd012202e4
CodeAWProject <activityware@gmail.com>
2023-11-08 19:14:03 +0100

    Defining relationships between models of Invoice, Company and User

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
     use HasFactory;
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
 }
+
+
