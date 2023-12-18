6a4afca331228feb52c9ebba9b3a8bff88fec9ad
CodeAWProject <activityware@gmail.com>
2023-12-18 23:16:00 +0100

    Implementing a 'create' method through an API call to the Tasks API App

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Session;
+use GuzzleHttp\Client;
 
 class TaskController extends Controller
 {
@@ -29,7 +30,40 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'priority' => 'required',
+            'is_completed' => 'required',
+            'user_id' => 'required',      
+        ]);
+
+        $acces_token = session()->get('access_token')[0];
+
+        $url = 'http://localhost/APICoursePHP/todolistAPI/api/tasks';
+        $headers = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $acces_token,
+            ]
+        ];
+
+        $data = [
+            'name' => $request->input('name'),
+            'priority' => $request->input('priority'),
+            'is_completed' => $request->input('is_completed'),
+            'user_id' => $request->input('user_id'),
+        ];
+
+        $client = new Client();
+
+        $response = $client->post($url, $headers + ['json' => $data]);
+
+        $responseData = json_decode($response->getBody(), true);
+
+        
+
+        
+        return redirect()->route('tasks.index');
     }
 
     /**
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -16,7 +16,30 @@
 
             <x-modal name="task">
                 <x-slot:body>
-                    <span class="px-5">Create new task</span>
+                    <form action="{{route('tasks.store')}}" method="POST">
+                        @csrf
+                
+                        <div class="mb-8 p-8">
+                            <label for="name">Naam</label>
+                            <x-text-input name="name" type="text"></x-text-input>
+                
+                            <label for="priority">Prioriteit</label>
+                            <x-text-input name="priority" type="number"></x-text-input>
+
+                            <label for="is_completed"></label>
+                            <select name="is_completed">
+                                <option value="0">Nee</option>
+                                <option value="1">Ja</option>
+                            </select>
+                
+                            <label for="user_id">User ID</label>
+                            <x-text-input name="user_id" type="number"></x-text-input>
+                
+                            
+                        </div>
+                
+                        <x-button type="submit">Maak</x-button>
+                    </form>
                 </x-slot>
             </x-modal>
             
